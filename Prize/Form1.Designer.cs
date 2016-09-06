c86d35fb003e1f8d997e3ad1cce8f8ad51800aa3
riskteria <riskteria@gmail.com>
2016-09-06 11:20:10 -0700

    Added random generator

--- Prize/Form1.Designer.cs
+++ Prize/Form1.Designer.cs
@@ -28,23 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblEventName = new System.Windows.Forms.Label();
+            this.lblCompanyName = new System.Windows.Forms.Label();
+            this.lblRandomNumber = new System.Windows.Forms.Label();
+            this.btnRandToggler = new System.Windows.Forms.Button();
+            this.randTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // lblEventName
+            // 
+            this.lblEventName.AutoSize = true;
+            this.lblEventName.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEventName.Location = new System.Drawing.Point(352, 91);
+            this.lblEventName.Name = "lblEventName";
+            this.lblEventName.Size = new System.Drawing.Size(347, 74);
+            this.lblEventName.TabIndex = 0;
+            this.lblEventName.Tag = "";
+            this.lblEventName.Text = "Arisan Grosir";
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompanyName.Location = new System.Drawing.Point(326, 165);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(410, 96);
+            this.lblCompanyName.TabIndex = 1;
+            this.lblCompanyName.Text = "MONDELEZ";
+            // 
+            // lblRandomNumber
+            // 
+            this.lblRandomNumber.AutoSize = true;
+            this.lblRandomNumber.Font = new System.Drawing.Font("Courier New", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandomNumber.Location = new System.Drawing.Point(268, 336);
+            this.lblRandomNumber.Name = "lblRandomNumber";
+            this.lblRandomNumber.Size = new System.Drawing.Size(667, 154);
+            this.lblRandomNumber.TabIndex = 2;
+            this.lblRandomNumber.Text = "0 0 0 0";
+            this.lblRandomNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRandToggler
+            // 
+            this.btnRandToggler.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandToggler.Location = new System.Drawing.Point(424, 577);
+            this.btnRandToggler.Name = "btnRandToggler";
+            this.btnRandToggler.Size = new System.Drawing.Size(395, 107);
+            this.btnRandToggler.TabIndex = 3;
+            this.btnRandToggler.Text = "Mulai Acak";
+            this.btnRandToggler.UseVisualStyleBackColor = true;
+            this.btnRandToggler.Click += new System.EventHandler(this.btnRandToggler_Click);
+            // 
+            // randTimer
+            // 
+            this.randTimer.Interval = 1;
+            this.randTimer.Tick += new System.EventHandler(this.randTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(899, 606);
+            this.ClientSize = new System.Drawing.Size(1215, 606);
+            this.Controls.Add(this.btnRandToggler);
+            this.Controls.Add(this.lblRandomNumber);
+            this.Controls.Add(this.lblCompanyName);
+            this.Controls.Add(this.lblEventName);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arisan Grosir";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEventName;
+        private System.Windows.Forms.Label lblCompanyName;
+        private System.Windows.Forms.Label lblRandomNumber;
+        private System.Windows.Forms.Button btnRandToggler;
+        private System.Windows.Forms.Timer randTimer;
     }
 }
 
--- Prize/Form1.cs
+++ Prize/Form1.cs
@@ -12,30 +12,60 @@ namespace Prize
 {
     public partial class Form1 : Form
     {
-        private Label formLabel = new Label();
-        private Label randNumber = new Label();
-        private Button toggleBtn = new Button();
-
-        private void _initializeForm ()
-        {
-
-        }
-
-        private void _initalizeControls ()
-        {
-            this.Controls.Add(formLabel);
-            this.Controls.Add(randNumber);
-            this.Controls.Add(toggleBtn);
-        }
-
+        private Boolean togglerClicked;
+        private String angkaAcak;
+        
         public Form1()
         {
             InitializeComponent();
+            this.togglerClicked = false;
+            this.angkaAcak = "0 0 0 0";
+        }
+
+        private void _initForm ()
+        {
+            this.lblEventName.Left = (this.ClientSize.Width - this.lblEventName.Width) / 2;
+            this.lblCompanyName.Left = (this.ClientSize.Width - this.lblCompanyName.Width) / 2;
+            this.lblRandomNumber.Left = (this.ClientSize.Width - this.lblRandomNumber.Width) / 2;
+            this.btnRandToggler.Left = (this.ClientSize.Width - this.btnRandToggler.Width) / 2;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this._initForm();
+            this.randTimer.Enabled = false;
+            this.lblRandomNumber.Text = this.angkaAcak;
+        }
 
+        private void btnRandToggler_Click(object sender, EventArgs e)
+        {
+            if(this.togglerClicked)
+            {
+                this.btnRandToggler.Text = "Mulai Acak";
+                this.togglerClicked = false;
+                this.randTimer.Enabled = false;
+            }
+            else
+            {
+                this.btnRandToggler.Text = "Berhenti";
+                this.togglerClicked = true;
+                this.randTimer.Enabled = true;
+            }
+            
+        }
+
+        private void randTimer_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int rndNumber = rnd.Next(1, 1000);
+            String angkaFormat = String.Format("{0:0000}", rndNumber);
+
+            this.angkaAcak = String.Format("{0} {1} {2} {3}", 
+                angkaFormat[0],
+                angkaFormat[1],
+                angkaFormat[2],
+                angkaFormat[3]);
+            this.lblRandomNumber.Text = this.angkaAcak;
         }
     }
 }

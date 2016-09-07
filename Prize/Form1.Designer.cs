0d42d445c0c6704fe3b7f3b9279211ccba17b3aa
riskteria <riskteria@gmail.com>
2016-09-06 23:37:16 -0700

    Initalizing function

--- Prize/Form1.Designer.cs
+++ Prize/Form1.Designer.cs
@@ -34,11 +34,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(899, 606);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Arisan Grosir";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Prize/Form1.cs
+++ Prize/Form1.cs
@@ -12,9 +12,30 @@ namespace Prize
 {
     public partial class Form1 : Form
     {
+        private Label formLabel = new Label();
+        private Label randNumber = new Label();
+        private Button toggleBtn = new Button();
+
+        private void _initializeForm ()
+        {
+
+        }
+
+        private void _initalizeControls ()
+        {
+            this.Controls.Add(formLabel);
+            this.Controls.Add(randNumber);
+            this.Controls.Add(toggleBtn);
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
